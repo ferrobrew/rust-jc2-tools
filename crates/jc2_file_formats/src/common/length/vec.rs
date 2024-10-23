af6e3daa680318a62e88d31bef0afe402257531f
Josh Wood <sk83rjosh@gmail.com>
2024-10-23 21:59:29 +0200

    feat(jc2_file_formats): `LengthVec` now supports byte sized lengths

--- crates/jc2_file_formats/src/common/length/vec.rs
+++ crates/jc2_file_formats/src/common/length/vec.rs
@@ -6,17 +6,19 @@ use super::{BinReadWrite, LengthType};
 
 #[binrw]
 #[derive(Clone, Default, Debug, PartialEq, Eq)]
-pub struct LengthVec<T: BinReadWrite, L: LengthType> {
+pub struct LengthVec<T: BinReadWrite, L: LengthType, const B: bool = false> {
     #[br(parse_with = Self::parse)]
     #[bw(write_with = Self::write)]
     value: Vec<T>,
     marker: std::marker::PhantomData<L>,
 }
 
-impl<T: BinReadWrite, L: LengthType> LengthVec<T, L> {
+impl<T: BinReadWrite, L: LengthType, const B: bool> LengthVec<T, L, B> {
+    const WIDTH: usize = if B { std::mem::size_of::<T>() } else { 1 };
+
     #[binrw::parser(reader, endian)]
     fn parse() -> binrw::BinResult<Vec<T>> {
-        let count = <L as LengthType>::parse(reader, endian, ())?;
+        let count = <L as LengthType>::parse(reader, endian, ())? / Self::WIDTH;
         let mut result = Vec::with_capacity(count);
         for _ in 0..count {
             result.push(T::read_options(reader, endian, ())?);
@@ -26,7 +28,7 @@ impl<T: BinReadWrite, L: LengthType> LengthVec<T, L> {
 
     #[binrw::writer(writer, endian)]
     fn write(value: &Vec<T>) -> binrw::BinResult<()> {
-        <L as LengthType>::write(value.len(), writer, endian, ())?;
+        <L as LengthType>::write(value.len() * Self::WIDTH, writer, endian, ())?;
         for element in value {
             element.write_options(writer, endian, ())?;
         }
@@ -38,14 +40,14 @@ impl<T: BinReadWrite, L: LengthType> LengthVec<T, L> {
     }
 }
 
-impl<T: BinReadWrite, L: LengthType> AsRef<[T]> for LengthVec<T, L> {
+impl<T: BinReadWrite, L: LengthType, const B: bool> AsRef<[T]> for LengthVec<T, L, B> {
     #[inline]
     fn as_ref(&self) -> &[T] {
         &self.value
     }
 }
 
-impl<T: BinReadWrite, L: LengthType> Deref for LengthVec<T, L> {
+impl<T: BinReadWrite, L: LengthType, const B: bool> Deref for LengthVec<T, L, B> {
     type Target = Vec<T>;
 
     #[inline]
@@ -54,21 +56,21 @@ impl<T: BinReadWrite, L: LengthType> Deref for LengthVec<T, L> {
     }
 }
 
-impl<T: BinReadWrite, L: LengthType> DerefMut for LengthVec<T, L> {
+impl<T: BinReadWrite, L: LengthType, const B: bool> DerefMut for LengthVec<T, L, B> {
     #[inline]
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.value
     }
 }
 
-impl<T: BinReadWrite, L: LengthType> From<LengthVec<T, L>> for Vec<T> {
+impl<T: BinReadWrite, L: LengthType, const B: bool> From<LengthVec<T, L, B>> for Vec<T> {
     #[inline]
-    fn from(value: LengthVec<T, L>) -> Self {
+    fn from(value: LengthVec<T, L, B>) -> Self {
         value.value
     }
 }
 
-impl<T: BinReadWrite, L: LengthType> From<Vec<T>> for LengthVec<T, L> {
+impl<T: BinReadWrite, L: LengthType, const B: bool> From<Vec<T>> for LengthVec<T, L, B> {
     #[inline]
     fn from(value: Vec<T>) -> Self {
         Self {
@@ -78,7 +80,7 @@ impl<T: BinReadWrite, L: LengthType> From<Vec<T>> for LengthVec<T, L> {
     }
 }
 
-impl<T: BinReadWrite + Clone, L: LengthType> From<&[T]> for LengthVec<T, L> {
+impl<T: BinReadWrite + Clone, L: LengthType, const B: bool> From<&[T]> for LengthVec<T, L, B> {
     #[inline]
     fn from(value: &[T]) -> Self {
         Self {
