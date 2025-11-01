f70ac3ec43df79b40f5c5dbdf06d5899f88a4e61
Josh Wood <sk83rjosh@gmail.com>
2025-11-01 22:31:19 +0100

    chore(jc2_file_formats): rename `Endian` to `RenderBlockModelEndian`

--- crates/jc2_file_formats/src/render_block_model/mod.rs
+++ crates/jc2_file_formats/src/render_block_model/mod.rs
@@ -1,9 +1,9 @@
 use std::io::{Read, Seek, Write};
 
+use binrw::{BinRead, BinWrite, binrw};
 use thiserror::Error;
 
 use crate::math::Vec3;
-use binrw::{BinRead, BinWrite, binrw};
 
 mod render_block;
 pub use render_block::*;
@@ -14,41 +14,19 @@ pub use common::*;
 mod vertex_format;
 pub use vertex_format::*;
 
-#[derive(Error, Debug)]
-pub enum RenderBlockError {
-    #[error("invalid vertex format (expected {expected:?}, found {found:?}")]
-    InvalidVertexFormat {
-        expected: VertexFormat,
-        found: VertexFormat,
-    },
-    #[error("invalid length")]
-    InvalidArrayLength,
-    #[error("invalid block footer")]
-    InvalidBlockFooter,
-}
-
-#[binrw]
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum Endian {
-    #[brw(magic = b"\x05\x00\x00\x00")]
-    Little,
-    #[brw(magic = b"\x00\x00\x00\x05")]
-    Big,
-}
-
 #[binrw]
 #[derive(Clone, Debug)]
 pub struct RenderBlockModel {
-    pub endian: Endian,
+    pub endian: RenderBlockModelEndian,
     #[brw(magic = b"RBMDL")]
     #[brw(assert(version.x == 1), assert(version.y == 13))]
-    #[brw(is_little(matches!(endian, Endian::Little)))]
+    #[brw(is_little(matches!(endian, RenderBlockModelEndian::Little)))]
     pub version: Vec3<u32>,
-    #[brw(is_little(matches!(endian, Endian::Little)))]
+    #[brw(is_little(matches!(endian, RenderBlockModelEndian::Little)))]
     pub min: Vec3<f32>,
-    #[brw(is_little(matches!(endian, Endian::Little)))]
+    #[brw(is_little(matches!(endian, RenderBlockModelEndian::Little)))]
     pub max: Vec3<f32>,
-    #[brw(is_little(matches!(endian, Endian::Little)))]
+    #[brw(is_little(matches!(endian, RenderBlockModelEndian::Little)))]
     pub blocks: RenderBlocks,
 }
 
@@ -68,4 +46,26 @@ impl RenderBlockModel {
         #[cfg(target_endian = "big")]
         return self.write_be(writer);
     }
+}
+
+#[binrw]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum RenderBlockModelEndian {
+    #[brw(magic = b"\x05\x00\x00\x00")]
+    Little,
+    #[brw(magic = b"\x00\x00\x00\x05")]
+    Big,
+}
+
+#[derive(Error, Debug)]
+pub enum RenderBlockError {
+    #[error("invalid vertex format (expected {expected:?}, found {found:?}")]
+    InvalidVertexFormat {
+        expected: VertexFormat,
+        found: VertexFormat,
+    },
+    #[error("invalid length")]
+    InvalidArrayLength,
+    #[error("invalid block footer")]
+    InvalidBlockFooter,
 }
